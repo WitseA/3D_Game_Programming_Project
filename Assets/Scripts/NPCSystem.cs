3d471ff21d6622c0672e3b81e5e450b6d36086d0
WitseA <witseaudenaert@gmail.com>
2024-01-23 15:21:14 +0100

    Working dialogue mechanic

--- Assets/Scripts/NPCSystem.cs
+++ Assets/Scripts/NPCSystem.cs
@@ -8,22 +8,19 @@ public class NPCSystem : MonoBehaviour
     public bool playerDetection = false;
     public GameObject beeld;
     public static bool inDialogue { get; set; } = false;
-    public List<string> dialogues { get; private set; } = new List<string>();
     private int index = 0;
 
     void Start()
     {
-        // Set up dialogues
-        dialogues.Add("This is the first line of dialogue.");
-        dialogues.Add("Here's the second line.");
-        dialogues.Add("And the third line.");
+        // Set up first line
+        beeld.GetComponentInChildren<TextMeshProUGUI>().text = "Ni tonen den deze";
     }
 
     void Update()
     {
         if (playerDetection && Input.GetKeyDown(KeyCode.F) && !inDialogue)
         {
-            beeld.SetActive(true);
+            
             inDialogue = true;
 
             // Start the dialogue sequence
@@ -31,32 +28,32 @@ public class NPCSystem : MonoBehaviour
         }
     }
 
-    private void ShowNextDialogue()
-    {
-        if (index < dialogues.Count)
-        {
-            // Update the dialogue text on the canvas
-            TextMeshProUGUI textMeshPro = beeld.GetComponentInChildren<TextMeshProUGUI>();
-            if (textMeshPro != null)
-            {
-                textMeshPro.text = dialogues[index];
-            }
-            else
-            {
-                Debug.LogError("TextMeshProUGUI component not found on the dialogue object.");
-            }
+    //private void ShowNextDialogue()
+    //{
+    //    if (index < dialogues.Count)
+    //    {
+    //        // Update the dialogue text on the canvas
+    //        TextMeshProUGUI textMeshPro = beeld.GetComponentInChildren<TextMeshProUGUI>();
+    //        if (textMeshPro != null)
+    //        {
+    //            textMeshPro.text = dialogues[index];
+    //        }
+    //        else
+    //        {
+    //            Debug.LogError("TextMeshProUGUI component not found on the dialogue object.");
+    //        }
 
-            index += 1;
-            print(index + " en " + dialogues.Count);
-            if (index > dialogues.Count)
-            {
-                // All dialogues displayed, end dialogue
-                inDialogue = false;
-                index = 0;
-                beeld.SetActive(false);
-            }
-        }
-    }
+    //        index += 1;
+    //        print(index + " en " + dialogues.Count);
+    //        if (index > dialogues.Count)
+    //        {
+    //            // All dialogues displayed, end dialogue
+    //            inDialogue = false;
+    //            index = 0;
+    //            beeld.SetActive(false);
+    //        }
+    //    }
+    //}
 
     private void OnTriggerEnter(Collider other)
     {
--- Assets/Scripts/NewDialogue.cs
+++ Assets/Scripts/NewDialogue.cs
@@ -6,39 +6,48 @@ using UnityEngine;
 public class NewDialogue : MonoBehaviour
 {
     private NPCSystem npcSystem;
+    private int index = 0;
+    private List<string> dialogues = new List<string>();
 
     void Start()
     {
         // Find the NPCSystem instance in the scene
         npcSystem = FindObjectOfType<NPCSystem>();
+        dialogues.Add("1");
+        dialogues.Add("2");
+        dialogues.Add("3");
     }
 
     void Update()
     {
         if (npcSystem != null && NPCSystem.inDialogue)
         {
-            int i = 0;
-            while (i < npcSystem.dialogues.Count)
+            TextMeshProUGUI textMeshPro = npcSystem.beeld.GetComponentInChildren<TextMeshProUGUI>();
+            textMeshPro.text = dialogues[index];
+            npcSystem.beeld.SetActive(true);
+
+            if (index < dialogues.Count && Input.GetMouseButtonDown(0))
             {
-                print("Nu dialoog: " + i);
+                index++;
+
                 // Update the dialogue text on the canvas
-                TextMeshProUGUI textMeshPro = npcSystem.beeld.GetComponentInChildren<TextMeshProUGUI>();
-                if (textMeshPro != null)
+                if (textMeshPro != null && index < dialogues.Count)
                 {
-                    textMeshPro.text = npcSystem.dialogues[i];
+                    textMeshPro.text = dialogues[index];
                 }
-                else
+                else if (textMeshPro == null)
                 {
                     Debug.LogError("TextMeshProUGUI component not found on the dialogue object.");
                 }
-                if (Input.GetMouseButtonDown(0))
+                
+                if (index >= dialogues.Count)
                 {
-                    i++;
+                    // All dialogues displayed, end dialogue
+                    NPCSystem.inDialogue = false;
+                    index = 0;
+                    npcSystem.beeld.SetActive(false);
                 }
             }
-            // All dialogues displayed, end dialogue
-            NPCSystem.inDialogue = false;
-            npcSystem.beeld.SetActive(false);
         }
     }
 }
